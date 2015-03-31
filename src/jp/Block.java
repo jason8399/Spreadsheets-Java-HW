eec81be407734e7ac3eddb28d85168ebeeaab66e
JasonPan <jason8399@gmail.com>
2015-03-31 15:58:23 +0800

    add class Block

---
+++ src/jp/Block.java
@@ -0,0 +1,22 @@
+package jp;
+
+
+/**
+ * Created by JasonPan on 3/31/15.
+ */
+public class Block {
+    private int number;
+    private String command;
+
+    public Block(){
+
+    }
+
+    public Block(int input) {
+
+    }
+
+    public Block(String input) {
+
+    }
+}
